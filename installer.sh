55f46153908f4d1dbcbdbbd3a789ab87f631c30d
Samy <support@a2-sploit.tk>
2020-06-03 12:57:33 +0000

    Fixing Installer Issue For Ubuntu

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 echo "note : we are going to update the system repository + upgrade the system"
 echo "note : we are also going to install some nessacary packages"
 apt-get update
-apt install -y python python2 ruby fish bash php figlet toilet cowsay
+apt install -y python fish bash php figlet toilet cowsay
 apt upgrade -y
  ###############
 echo "Making NgRok Executable";
